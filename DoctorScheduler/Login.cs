6e7a506410db109e7d22e4a4f523dfb907769768
balaz.keresi <kobalu@gmail.com>
2021-12-18 19:01:44 +0100

    Creating login reader

---
+++ DoctorScheduler/Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorScheduler
+{
+    class Login
+    {
+        string loggedInUserName;
+        string loggedInPassword;
+        public void LogIn()
+        {
+            Console.Write("Username: "); string username = Console.ReadLine();
+            Console.Write("Password: "); string password = HiddenInputReader.ReadLine();
+            Console.WriteLine();
+
+            if (UserExists(username, password))
+            {
+                loggedInUserName = username;
+                loggedInPassword = password;
+                Console.WriteLine("Welcome {0}", username);
+            }
+            else 
+            {
+                Console.WriteLine("Login failed. Wrong username or password");
+            }
+        }
+
+        bool UserExists(string username, string password)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ DoctorScheduler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DoctorScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login();
+            login.LogIn();
+        }
+    }
+}
